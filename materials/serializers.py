02cc929cfc6d63dc3cb3a5983ccf70b3a9c47c06
Liudmila Beloritskaya <meiramirth@gmail.com>
2024-11-18 11:13:30 +0300

    add felds course_lesson_count, lesson_titles

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from materials.models import Course, Lesson
 
@@ -7,6 +7,21 @@ class CourseSerializer(ModelSerializer):
     class Meta:
         model = Course
         fields = "__all__"
+
+
+class CourseDetailSerializer(ModelSerializer):
+    course_lesson_count = SerializerMethodField()
+    lesson_titles = SerializerMethodField()
+
+    def get_course_lesson_count(self, course):
+        return course.lessons.count()
+
+    def get_lesson_titles(self, course):
+        return [lesson.title for lesson in course.lessons.all()]
+
+    class Meta:
+        model = Course
+        fields = ("title", "description", "course_lesson_count", "lesson_titles")
 
 
 class LessonSerializer(ModelSerializer):
