2d4e86e9b1d37342e2a8b257bdad1d8fb6fec7a8
Meira Beloritskaya <meiramirth@gmail.com>
2025-01-14 21:30:13 +0200

    change settings

--- config/settings.py
+++ config/settings.py
@@ -91,9 +91,9 @@ else:  # Локальный режим (Docker)
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.getenv("POSTGRES_DB"),
-            "USER": os.getenv("POSTGRES_USER"),
-            "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+            "NAME": 'postgres',
+            "USER": "postgres",
+            "PASSWORD": "postgres",
             "HOST": os.getenv("POSTGRES_HOST", "localhost"),
             "PORT": os.getenv("POSTGRES_PORT"),
             "TEST": {
